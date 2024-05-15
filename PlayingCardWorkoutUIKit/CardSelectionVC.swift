aa0455bd450aeb935e36641273cd1c9dd91bf553
Scott Crowder <scrowder@gmail.com>
2024-05-15 17:30:06 -0500

    create deck of cards. build timer to change displayed card. build restart button to restart timer. build stop button to stop timer.

--- PlayingCardWorkoutUIKit/CardSelectionVC.swift
+++ PlayingCardWorkoutUIKit/CardSelectionVC.swift
@@ -10,24 +10,38 @@ import UIKit
 class CardSelectionVC: UIViewController {
     
     @IBOutlet var cardImageView: UIImageView!
-    
     @IBOutlet var buttons: [UIButton]!
+    
+    var cards: [UIImage] = Deck.allValues
+    
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        stopButton.layer.cornerRadius       = 8
-//        restartButton.layer.cornerRadius    = 8
-//        rulesButton.layer.cornerRadius      = 8
+        startTimer()
         for button in buttons {
             button.layer.cornerRadius = 8
         }
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
     }
     @IBAction func restartButtonTapped(_ sender: Any) {
-    }
-    @IBAction func rulesButtonTapped(_ sender: Any) {
+        timer.invalidate()
+        startTimer()
     }
 }
