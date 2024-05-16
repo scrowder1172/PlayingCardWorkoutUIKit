9ffe0907a5d0d1eb44d6c5bbee859013b5033cb0
Scott Crowder <scrowder@gmail.com>
2024-05-15 22:03:45 -0500

    enable rules button to show rules vc when tapped

--- PlayingCardWorkoutUIKit/CardSelectionVC.swift
+++ PlayingCardWorkoutUIKit/CardSelectionVC.swift
@@ -65,6 +65,7 @@ class CardSelectionVC: UIViewController {
     
     func configureRulesButton() {
         view.addSubview(rulesButton)
+        rulesButton.addTarget(self, action: #selector(presentRulesVC), for: .touchUpInside)
         
         NSLayoutConstraint.activate([
             rulesButton.widthAnchor.constraint(equalToConstant: 115),
@@ -72,6 +73,10 @@ class CardSelectionVC: UIViewController {
             rulesButton.trailingAnchor.constraint(equalTo: stopButton.trailingAnchor),
             rulesButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 30),
         ])
+    }
+    
+    @objc func presentRulesVC() {
+        present(RulesVC(), animated: true)
     }
 
 }
